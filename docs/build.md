78ee065213adbadc2cb91d9882c7cccf5c152a87
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-05-17 03:46:57 +0300

    packaging: AppImage and Flatpak CI (#20)
    - add appimage CI (sharun)
    - add flatpak ci
    - add docs/build.md
    
    ---------
    
    Co-authored-by: reakjra <mattia.111123@gmail.com>

---
+++ docs/build.md
@@ -0,0 +1,46 @@
+# Building
+
+Disclaimer: This was written with arch in mind. So the package names might be different or some might not even exist on your distro.
+
+1. Install the dependencies;
+```
+sudo pacman -S pkg-config \
+cmake \
+systemd-libs \
+wget \
+protobuf \
+rust \
+cargo \
+libxkbcommon \
+xcb-util-cursor \
+libcups \
+glib2 \
+libproxy \
+qt6-base \
+qt6-tools \
+qt6-wayland \
+qt6-shadertools \
+qt6-declarative \
+qt6-5compat \
+qt6-svg
+
+# On some distros
+libqt6waylandclient6
+qml6-module-qtwayland-compositor
+libqt6core5compat6
+libqt6core5compat6-dev
+qt6-base-private-dev
+```
+
+2. Add `/usr/lib/qt6/bin` to your `$PATH` if needed
+
+```sh
+export PATH="/usr/lib/qt6/bin:$PATH"
+```
+
+3. Compile the app
+```sh
+cargo build --release -p omikuji
+```
+
+4.Run `./packaging/make-appimage.sh` to make an appimage.
---
+++ packaging/make-appimage.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+if [ -n "${GITHUB_REF_NAME:-}" ] && [ "${GITHUB_REF_TYPE:-}" = "tag" ]; then
+    VERSION="${GITHUB_REF_NAME}"
+else
+    VERSION=$(pacman -Q omikuji-git | awk '{print $2; exit}')
+fi
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/512x512/apps/io.github.reakjra.omikuji.png
+export DESKTOP=/usr/share/applications/io.github.reakjra.omikuji.desktop
+
+#Remove leftovers
+rm -rf AppDir dist
+
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+
+# Deploy dependencies
+./quick-sharun /usr/bin/omikuji
+
+# Turn AppDir into AppImage
+./quick-sharun --make-appimage
