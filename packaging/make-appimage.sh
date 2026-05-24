ebc94dd60f53b48568cab45ee5c880b48a870aa6
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-05-24 13:18:13 +0300

    Make use of debloated pkgs (#54)

--- packaging/make-appimage.sh
+++ packaging/make-appimage.sh
@@ -4,6 +4,7 @@ set -eu
 
 ARCH=$(uname -m)
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+DEBLOATED_PKGS="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 if [ -n "${GITHUB_REF_NAME:-}" ] && [ "${GITHUB_REF_TYPE:-}" = "tag" ]; then
     VERSION="${GITHUB_REF_NAME}"
@@ -21,8 +22,12 @@ export DESKTOP=/usr/share/applications/io.github.reakjra.omikuji.desktop
 rm -rf AppDir dist
 
 # ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS" -O ./get-debloated-pkgs
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
+chmod +x ./quick-sharun ./get-debloated-pkgs
+
+# Debloated pkgs
+./get-debloated-pkgs --add-common --prefer-nano
 
 # Deploy dependencies
 ./quick-sharun /usr/bin/omikuji
